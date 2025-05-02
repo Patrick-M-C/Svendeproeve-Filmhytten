31fcd288a95236bfc9e5499f1d6928098a9d1ac4
Patrick-M-C <84278175+Patrick-M-C@users.noreply.github.com>
2025-05-02 11:05:57 +0200

    Model classes sat op

--- Svendeprøve/Svendeprøve/Components/Models/Ticket.cs
+++ Svendeprøve/Svendeprøve/Components/Models/Ticket.cs
@@ -1,6 +1,12 @@
 ﻿namespace Svendeprøve.Components.Models
 {
-    public class Ticket
+    public class User
     {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
     }
 }
