c23ff14c2c5909409ed386f204f87bc4cbfabdf2
Kasper <kasperflodin@gmail.com>
2025-05-07 10:47:17 +0200

    Hall seat og user controller

--- Svendeprøve.Api/Controllers/HallController.cs
+++ Svendeprøve.Api/Controllers/HallController.cs
@@ -1,12 +1,110 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Svendeprøve.Repo.DatabaseContext;
+using Svendeprøve.Repo.DTO;
+using Svendeprøve.Repo.Interface;
 
 namespace Svendeprøve.Api.Controllers
 {
     public class HallController : Controller
     {
-        public IActionResult Index()
+        private readonly Databasecontext _context;
+        IHall Hall { get; set; }
+
+        public HallController(Databasecontext context, IHall hall)
         {
-            return View();
+            _context = context;
+            this.Hall = hall;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hall>>> GetHalls()
+        {
+            return await _context.Hall.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hall>> GetHallbyid(int id)
+        {
+            var hall = await _context.Hall.FindAsync(id);
+
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return hall;
+        }
+
+        [HttpGet("withseat{id}")]
+        public async Task<ActionResult<Hall>> GetHallWithSeatsbyid(int id)
+        {
+            var hall = await _context.Hall.Include(h => h.Seats).FirstOrDefaultAsync(h => h.Id == id);
+
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return hall;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Hall>> CreateHall(Hall hall)
+        {
+            _context.Hall.Add(hall);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetHallbyid), new { id = hall.Id }, hall);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateHall(int id, Hall hall)
+        {
+            if (id != hall.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hall).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HallExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHall(int id)
+        {
+            var hall = await _context.Hall.FindAsync(id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hall.Remove(hall);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HallExists(int id)
+        {
+            return _context.Hall.Any(e => e.Id == id);
         }
     }
 }
--- Svendeprøve.Api/Controllers/UserController.cs
+++ Svendeprøve.Api/Controllers/UserController.cs
@@ -1,12 +1,122 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Svendeprøve.Repo.DatabaseContext;
+using Svendeprøve.Repo.DTO;
 
 namespace Svendeprøve.Api.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly Databasecontext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UserController(Databasecontext context, IPasswordHasher<User> passwordHasher)
         {
-            return View();
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        // GET: api/User
+        [HttpGet("withTickets")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsersIncludeTickets()
+        {
+            return await _context.User.Include(u => u.Tickets).ToListAsync();
+        }
+
+        // GET: api/User/{id} 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            if (user == null) return BadRequest("User cannot be null.");
+
+            user.Tickets = null;
+            user.Password = _passwordHasher.HashPassword(user, user.Password);
+
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT: api/User/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest("User ID mismatch.");
+            }
+
+            var existingUser = await _context.User.FindAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            // Update user properties
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            existingUser.PhoneNumber = user.PhoneNumber;
+
+            existingUser.Password = _passwordHasher.HashPassword(user, user.Password);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/User/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.Id == id);
         }
     }
 }
