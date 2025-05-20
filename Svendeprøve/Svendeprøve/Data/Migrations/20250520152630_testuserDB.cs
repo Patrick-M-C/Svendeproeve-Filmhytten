e28bbc7d4b1e9507f109bb26da4b8c2b69a33ce4
Patrick-M-C <84278175+Patrick-M-C@users.noreply.github.com>
2025-05-20 17:39:34 +0200

    flere QOL løsninger

---
+++ Svendeprøve/Svendeprøve/Data/Migrations/20250520152630_testuserDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Svendeprøve.Migrations
+{
+    /// <inheritdoc />
+    public partial class testuserDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
